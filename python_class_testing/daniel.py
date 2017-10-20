375d38aee9dc88490d7b141cf000183c278cc386
danienl1 <le.daniel.nam@gmail.com>
2017-10-20 01:23:41 -0700

    Update

--- python_class_testing/daniel.py
+++ python_class_testing/daniel.py
@@ -8,11 +8,19 @@ class Test():
 
     def print_age(self,username):
         print(self.database.data_list["users"][username]["age"])
+        
+    def test_add(self, username):
+        self.database.add_user(username)
+        
+        print(self.database.data_list)
+        ##   print(self.database.data_list[i])
 
 if __name__ == '__main__':
     database_object = Database("accounts")
     database_object.load_json_database()
     
     bot = Test(database_object)
-    bot.print_age("chayuso#5309")
+    #bot.print_age("chayuso#5309")
+    
+    bot.test_add("Daniel")
     
